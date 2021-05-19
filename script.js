4d9a286b1f4fda252a39f575ec34821b7a099dc2
Fernando Rodriguez Agredano <Frodri300@west-mec.local>
2021-05-19 16:36:27 -0700

    github problems

--- script.js
+++ script.js
@@ -328,13 +328,13 @@ $(function(){
     let $day;
     window.stop();
 
-    // (function() {
-    //     let link = document.createElement('link');
-    //     link.type = 'image/x-icon';
-    //     link.rel = 'shortcut icon';
-    //     link.href = '';
-    //     document.getElementsByTagName('head')[0].appendChild(link);
-    // })();
+    (function() {
+        let link = document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'shortcut icon';
+        link.href = 'favicon.png';
+        document.getElementsByTagName('head')[0].appendChild(link);
+    })();
 
     //Basic
     let $general = $(`#basic`);
